5d07f5807026cdde34174ffa6e947245721775b8
ruijosecj <ruijosecj@gmail.com>
2020-09-15 09:07:07 -0300

    GameDTO, GameService

--- backend/src/main/java/com/devsuperior/repositories/GameRepository.java
+++ backend/src/main/java/com/devsuperior/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.devsuperior.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long>{
 
 }
---
+++ backend/src/main/java/com/devsuperior/services/GameService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dto.GameDTO;
+import com.devsuperior.entities.Game;
+import com.devsuperior.repositories.GameRepository;
+
+@Service
+public class GameService {
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameDTO> findAll(){
+		List<Game> list = gameRepository.findAll();
+		return list.stream().map(x -> new GameDTO(x)).collect(Collectors.toList());
+	}
+}
